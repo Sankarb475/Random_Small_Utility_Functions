02ddbb7bdd1689abce968676e7614c53b8b9ed11
Sankar Biswas <sankarb475@gmail.com>
2018-12-14 23:27:59 +0530

    Create PowerFunction**.scala

---
+++ PowerFunction**.scala
@@ -0,0 +1,8 @@
+import scala.math.pow
+
+implicit class PowerFunction**(i: Int) {
+    def ** (b: Int): Int = pow(i, b).intValue
+}
+
+//output will be 3125, ** now works similar to pow function and being implicits in nature doesnt require instantiation
+val a = 5**5
