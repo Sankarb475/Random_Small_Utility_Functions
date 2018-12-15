b9ed8dd5920f185922492df682e7636db671574f
Sankar Biswas <sankarb475@gmail.com>
2018-12-15 15:02:21 +0530

    Update ScalaLearnings.scala

--- ScalaLearnings.scala
+++ ScalaLearnings.scala
@@ -1,3 +1,20 @@
 //Defining two dimensional array(10 * 10) with 'null' as default value
 
 var myArray = Array.ofDim[String](10, 10)
+
+//Generating all the permutation subsets of a list
+
+val xs = List( 'a', 'b' , 'c' , 'd' , 'e','a')
+val output = (1 to xs.length flatMap (x => xs.combinations(x))) map ( x => x.mkString(""))
+
+/*output ==> output1: Seq[String] = Vector(a, b, c, d, e, aa, ab, ac, ad, ae, bc, bd, be, cd, ce, de, aab, aac, aad, aae, 
+abc, abd, abe, acd, ace, ade, bcd, bce, bde, cde, aabc, aabd, aabe, aacd, aace, aade, abcd, abce, abde, acde, bcde, aabcd, 
+aabce, aabde, aacde, abcde, aabcde)
+*/
+def combine(in: List[Char]): Seq[String] = 
+    for {
+        len <- 1 to in.length
+        combinations <- in combinations len
+    } yield combinations.mkString 
+
+combine(xs) // Gives the same output
