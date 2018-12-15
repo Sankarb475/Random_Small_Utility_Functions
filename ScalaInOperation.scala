bec3b8a6710800b97727ad0b7996d6e775e34cae
Sankar Biswas <sankarb475@gmail.com>
2018-12-15 11:07:27 +0530

    Update and rename ScalaInFunction.scala to ScalaInOperation.scala

--- ScalaInOperation.scala
+++ ScalaInOperation.scala
@@ -1,9 +1,8 @@
 //Scala equivalent of Python's “in” operator which doesnt exist in Scala
 
-implicit class InOperation[T](v: T) extends AnyVal { 
-  def in(s: Set[T]) = { s contains v } 
+implicit class ScalaInOperation[T](v: T) { 
+  def in(s: Set[T]) = { s contains v }
 }
-
 
 val x = Set(1,2,3)
 
