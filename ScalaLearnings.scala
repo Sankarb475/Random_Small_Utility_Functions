f9830da8fec64337978e52e8218e937dd8d2c4df
Sankar Biswas <sankarb475@gmail.com>
2018-12-15 18:50:07 +0530

    Update ScalaLearnings.scala

--- ScalaLearnings.scala
+++ ScalaLearnings.scala
@@ -1,10 +1,12 @@
 //Defining two dimensional array(10 * 10) with 'null' as default value
+//==========================================================================
 
 var myArray = Array.ofDim[String](10, 10)
 val a = ArrayBuffer.fill(10,10)(null)
 val a : ArrayBuffer[ArrayBuffer[String]] = ArrayBuffer.fill(10,10)("null")
 
 //Generating all the combination subsets of a list
+//==========================================================================
 
 val xs = List( 'a', 'b' , 'c' , 'd' , 'e','a')
 val output = (1 to xs.length flatMap (x => xs.combinations(x))) map ( x => x.mkString(""))
@@ -20,3 +22,18 @@ def combine(in: List[Char]): Seq[String] =
     } yield combinations.mkString 
 
 combine(xs) // Gives the same output
+
+//Sorting two collection
+//==========================================================================
+val l1 = List(1,2,3) 
+val l2 = List(2,3,1)
+
+//If what you want is "these lists contain the same elements, irrespective of order or repetitions":
+l1.toSet == l2.toSet  //output = true , because there are no duplicate elements
+
+//If what you want is "these lists contain the same elements, and with the same number of repetitions of each":
+l1.sorted == l2.sorted  //output = true
+
+//If what you want is "these lists contain the same elements and are the same size, but the number of repetitions of a 
+//given element can differ between the two lists":
+l1.size == l2.size && l1.toSet == l2.toSet
