eb8851f53a58e06f01aff82ec3599c2200a9f63c
Sankar Biswas <sankarb475@gmail.com>
2018-12-27 16:56:23 +0530

    Update sparkLearnings.scala

--- sparkLearnings.scala
+++ sparkLearnings.scala
@@ -1,2 +1,20 @@
 //Spark UDF
-User-Defined Functions (aka UDF) is a feature of Spark SQL to define new Column-based functions 
+//User-Defined Functions (aka UDF) is a feature of Spark SQL to define new Column-based functions 
+//upperUDF is a user defined function
+import org.apache.spark.sql.functions.udf
+
+scala> val dataset = Seq((0, "hello"), (1, "world")).toDF("id", "text")
+dataset: org.apache.spark.sql.DataFrame = [id: int, text: string]
+
+scala> val upperUDF = udf(upper)
+upperUDF: org.apache.spark.sql.expressions.UserDefinedFunction = UserDefinedFunction(<function1>,StringType,Some(List(StringType)))
+
+scala> dataset.withColumn("upper", upperUDF($"text")).show
++---+-----+-----+
+| id| text|upper|
++---+-----+-----+
+|  0|hello|HELLO|
+|  1|world|WORLD|
++---+-----+-----+
+
+
