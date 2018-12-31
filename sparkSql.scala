3f5d209f97559d90c6c382a592efe8b4061ae756
Sankar Biswas <sankarb475@gmail.com>
2018-12-31 20:37:24 +0530

    Create sparkSql.scala

---
+++ sparkSql.scala
@@ -0,0 +1,19 @@
+scala> val df = Seq(1,2,3,4,5).toDF("ID")
+df: org.apache.spark.sql.DataFrame = [ID: int]
+
+scala> val dataFrame = df.withColumn("Age",lit(25))
+dataFrame: org.apache.spark.sql.DataFrame = [ID: int, Age: int]
+
+scala> 
+
+scala> dataFrame.show
++---+---+
+| ID|Age|
++---+---+
+|  1| 25|
+|  2| 25|
+|  3| 25|
+|  4| 25|
+|  5| 25|
++---+---+
+
