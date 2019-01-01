7e49dbd3af4318603e81c7a6623f6c0d8a5b1bc5
Sankar Biswas <sankarb475@gmail.com>
2019-01-01 14:44:56 +0530

    Update sparkSql.scala

--- sparkSql.scala
+++ sparkSql.scala
@@ -15,3 +15,51 @@ scala> dataFrame.show
 |  5| 25|
 +---+---+
 
+scala> val dfDummy = dataFrame.cache()
+dfDummy: dataFrame.type = [ID: int, Age: int]
+
+scala> dfDummy.show
++---+---+
+| ID|Age|
++---+---+
+|  1| 25|
+|  2| 25|
+|  3| 25|
+|  4| 25|
+|  5| 25|
++---+---+
+
+
+scala> dfDummy.printSchema
+root
+ |-- ID: integer (nullable = false)
+ |-- Age: integer (nullable = false)
+
+scala> dfDummy.registerTempTable("TableDummy")
+warning: there was one deprecation warning; re-run with -deprecation for details
+
+//registerTempTable() has been deprecated and createOrReplaceTempView() / createTempView has been incorporated in its place. 
+//so basically if you run registerTempView(), in the background it will run the createOrReplaceTempView()
+
+
+scala> dfDummy.createOrReplaceTempView("TableDummy")
+
+scala> val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+warning: there was one deprecation warning; re-run with -deprecation for details
+sqlContext: org.apache.spark.sql.SQLContext = org.apache.spark.sql.SQLContext@38f9e69b
+
+scala> val data = sqlContext.sql("select * from TableDummy where ID = 2")
+data: org.apache.spark.sql.DataFrame = [ID: int, Age: int]
+
+scala> data.show
++---+---+
+| ID|Age|
++---+---+
+|  2| 25|
++---+---+
+
+//If you want to import a Hive table into spark and use spark Sql
+
+
+
+
